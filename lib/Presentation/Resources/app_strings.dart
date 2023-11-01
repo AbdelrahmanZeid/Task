dbf849567e16c744b866817d64bcca5f2c95da36
Abdelrahman <zeid4643@gmail.com>
2023-11-01 14:27:08 +0200

    add dashboard view to material app

---
+++ lib/Presentation/Resources/app_strings.dart
@@ -0,0 +1,27 @@
+// class hold main strings  of app
+class AppStrings {
+  static const String home = 'Home';
+  static const String title = 'Multi-Services for Your Real Estate Needs';
+  static const String subTitle =
+      'Explore diverse real estate services for all your needs:\n property management,construction,insurance and \n more in one place';
+  static const String orderButtonText = 'Order';
+
+  static const String heyAhmed = 'Hey,Ahmed';
+  static const String categoriesButtonText = 'Categories';
+  static const String servicesButtonText = 'Services';
+  static const String ordersNumButtonText = 'Orders(0)';
+  static const String noOrderFoundTitle = 'No order found';
+  static const String noOrderFoundSubTitle =
+      'you can place your needed orders to let serve you.';
+
+  static const String assets = 'Assets';
+  static const String support = 'Support';
+  static const String profile = 'Profile';
+  static const String categoriesView = 'Categories View';
+  static const String seeAll = 'see all';
+  static const String constructions = 'Consturctions';
+  static const String insureance = 'Insureance';
+  static const String legal = 'Legal';
+  static const String buyAndSell = 'Buy & Sell';
+  static const String accountingServices = 'Accounting Services';
+}
