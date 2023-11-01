46512d22e4a2f9368b32268a2abf02b9ae83609b
Abdelrahman <zeid4643@gmail.com>
2023-11-01 14:25:28 +0200

    setup project and create strucure of app add assets manager and app string

---
+++ lib/Presentation/Resources/assets_manager.dart
@@ -0,0 +1,10 @@
+const String baseImagePath='assets/images';
+
+// Manage image of our app
+class AssetsManager{
+  
+ static const String manPresentation='$baseImagePath/02 Man Presentation Miniature Building.png';
+ static const String imageOne='$baseImagePath/Image (1).png';
+ static const String manImage='$baseImagePath/Man Image.png';
+ static const String emptyState='$baseImagePath/EmptyState.png';
+}
