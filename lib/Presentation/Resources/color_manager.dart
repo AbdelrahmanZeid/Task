2313b4ff1810440c80d640dcdc9b42955e38ce58
Abdelrahman <zeid4643@gmail.com>
2023-11-01 16:16:26 +0200

    finish color manager and add nav bar assets and category view assets

---
+++ lib/Presentation/Resources/color_manager.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// all color of app
+class ColorManager {
+  static Color appBarColor = const Color(0xffffffff);
+  static Color menuItemColor = const Color(0xff101010);
+  static Color mainColor = const Color(0xffD61116);
+  static Color searchIconColor = const Color(0xff8C8C8C);
+  static Color boundingBoxColor = const Color(0xffD9D9D9);
+  static Color titleColor = const Color(0xff101010);
+
+  static Color subTitleColor = const Color(0xff707070);
+  static Color categoryColorOne = const Color(0xffF2F2F2);
+  static Color seeAllColor = const Color(0xff8C8C8C);
+  static Color unActiveNavBar = const Color(0xffC4C4C4);
+  static Color middleFrameColor = const Color(0xffFFF5F5);
+}
