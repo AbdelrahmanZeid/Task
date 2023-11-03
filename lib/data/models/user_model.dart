e25879a1c4bdc3f622fd027f7ace87cb26b40901
Abdelrahman <zeid4643@gmail.com>
2023-11-03 14:54:04 +0200

    finish user model and create get user service

---
+++ lib/data/models/user_model.dart
@@ -0,0 +1,10 @@
+// class User {
+//   int? id;
+//   String? userName;
+
+//   User({required this.id,required this.userName});
+
+//   factory User.fromJson(Map<String,dynamic> jsonData){
+//     return User(id: userList[], userName: userName)
+//   }
+// }
