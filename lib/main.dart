51f2863b43e2ed7bd8412befb835bcbdf6436e1a
Abdelrahman <zeid4643@gmail.com>
2023-11-03 23:05:39 +0200

    update categories

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:task/application/app.dart';
 //starting point
 void main() => runApp(
-      const AppTask(),
+      const AppTask(), 
     );
